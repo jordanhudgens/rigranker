08624bcf890a197d528dadf31056911222d89ba2
Jordan Hudgens <jordan@wow.com>
2015-01-20 21:27:03 -0600

    integrated scoring algorithm

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -5,24 +5,31 @@ class Ranking < ActiveRecord::Base
   belongs_to :rig
 
   validates_presence_of :ranking_for_day, :rig_id
-end
 
-    t.boolean  "poor_event_sequence" => 1.20
-    t.boolean  "poor_comments" => 1.75
-    t.boolean  "blank_safety_meeting" => 1.00
-    t.boolean  "missing_rig_crew_travel_details" => 1.00
-    t.boolean  "not_using_device" => 3.00
-    t.boolean  "not_syncing_device" => 1.00
-    t.boolean  "missing_tubing_details" => 1.00
-    t.boolean  "poor_event_accuracy" => 1.50
-    t.boolean  "missing_events" => 1.25
-    t.boolean  "missing_shut_in_overnight_details" => 1.00
-    t.boolean  "missing_pressure_details" => 1.00
-    t.boolean  "poor_rig_up_rig_down_accuracy" => 2.00
-    t.boolean  "did_not_stop_timer" => 2.50
-    t.boolean  "missing_well_control_details" => 1.15
-    t.boolean  "poor_safety_personnel_details" => 1.30
-    t.boolean  "missing_waiting_on_details" => 3.00
-    t.boolean  "poor_well_control_comments" => 1.00
-    t.boolean  "missing_failure_details" => 1.50
-    t.boolean  "missing_tag_fill_details" => 1.00
+  before_create :generate_score
+
+  def generate_score
+    starting_score = 10.0
+    if self.poor_event_sequence then starting_score -= 1.20 end
+    if self.poor_comments then starting_score -= 1.75 end
+    if self.blank_safety_meeting then starting_score -= 1.00 end
+    if self.missing_rig_crew_travel_details then starting_score -= 1.00 end
+    if self.not_using_device then starting_score -= 3.00 end
+    if self.not_syncing_device then starting_score -= 1.00 end
+    if self.missing_tubing_details then starting_score -= 1.00 end
+    if self.poor_event_accuracy then starting_score -= 1.50 end
+    if self.missing_events then starting_score -= 1.25 end
+    if self.missing_shut_in_overnight_details then starting_score -= 1.00 end
+    if self.missing_pressure_details then starting_score -= 1.00 end
+    if self.poor_rig_up_rig_down_accuracy then starting_score -= 2.00 end
+    if self.did_not_stop_timer then starting_score -= 2.50 end
+    if self.missing_well_control_details then starting_score -= 1.15 end
+    if self.poor_safety_personnel_details then starting_score -= 1.30 end
+    if self.missing_waiting_on_details then starting_score -= 3.00 end
+    if self.poor_well_control_comments then starting_score -= 1.00 end
+    if self.missing_failure_details then starting_score -= 1.50 end
+    if self.missing_tag_fill_details then starting_score -= 1.00 end
+    self.score = starting_score
+  end
+
+end
