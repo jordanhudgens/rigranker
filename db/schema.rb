c69d2b4bdcaeed79fa708c76fd6c1516ad78addd
Jordan Hudgens <jordan@wow.com>
2015-01-23 18:36:40 -0600

    added name to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150118054850) do
+ActiveRecord::Schema.define(version: 20150124003612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 20150118054850) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "company_id"
+    t.string   "name"
   end
 
   add_index "users", ["company_id"], name: "index_users_on_company_id", using: :btree
